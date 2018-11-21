c671c9dfef6d88aa67301ef220599de52945c706
jesperqwop <jespertuan@hotmail.com>
2018-11-21 23:27:51 +0100

    moving platform(skyd mig)

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+    int current;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, Time.deltaTime * speed);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else {
+            current = (current + 1) % target.Length;
+        }
+    }
+}
---
+++ Assets/Scripts/Surface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surface : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = transform;
+
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+
+        }
+    }
+
+}
