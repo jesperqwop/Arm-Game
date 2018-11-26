0d1473167108c36cce9dba4d8cbfc770ba3e93dd
jesperqwop <jespertuan@hotmail.com>
2018-11-26 13:13:32 +0100

    ui

--- Assets/Scripts/WheelTurn.cs
+++ Assets/Scripts/WheelTurn.cs
@@ -1,49 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WheelTurn : MonoBehaviour
 {
-    public float speed;
+    public Sprite[] sprites;
     GameObject player;
-    GameObject wheel1;
-    GameObject wheel2;
+    GameObject attack1;
+    GameObject jump1;
+    GameObject shield1;
+    GameObject attack2;
+    GameObject jump2;
+    GameObject shield2;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        wheel1 = transform.GetChild(0).gameObject;
-        wheel2 = transform.GetChild(1).gameObject;
+        attack1 = transform.GetChild(0).gameObject;
+        jump1 = transform.GetChild(1).gameObject;
+        shield1 = transform.GetChild(2).gameObject;
+        attack2 = transform.GetChild(3).gameObject;
+        jump2 = transform.GetChild(4).gameObject;
+        shield2 = transform.GetChild(5).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        if (player.GetComponent<UNGA>().p1Arm == 1) {
-            wheel1.transform.rotation = Quaternion.RotateTowards(wheel1.transform.rotation, Quaternion.Euler(0, 0, 0),step);
+
+        if(player.GetComponent<UNGA>().p1Arm == 1) {
+            attack1.GetComponent<Image>().sprite = sprites[1];
+            jump1.GetComponent<Image>().sprite = sprites[2];
+            shield1.GetComponent<Image>().sprite = sprites[4];
         }
         if (player.GetComponent<UNGA>().p1Arm == 2)
         {
-            wheel1.transform.rotation = Quaternion.RotateTowards(wheel1.transform.rotation, Quaternion.Euler(0, 0, 120), step);
+            attack1.GetComponent<Image>().sprite = sprites[0];
+            jump1.GetComponent<Image>().sprite = sprites[3];
+            shield1.GetComponent<Image>().sprite = sprites[4];
         }
         if (player.GetComponent<UNGA>().p1Arm == 3)
         {
-            wheel1.transform.rotation = Quaternion.RotateTowards(wheel1.transform.rotation, Quaternion.Euler(0, 0, 240), step);
+            attack1.GetComponent<Image>().sprite = sprites[0];
+            jump1.GetComponent<Image>().sprite = sprites[2];
+            shield1.GetComponent<Image>().sprite = sprites[5];
         }
         if (player.GetComponent<UNGA>().p2Arm == 1)
         {
-            wheel2.transform.rotation = Quaternion.RotateTowards(wheel2.transform.rotation, Quaternion.Euler(0, 0, 0), step);
-        }
-        if (player.GetComponent<UNGA>().p2Arm == 2)
-        {
-            wheel2.transform.rotation = Quaternion.RotateTowards(wheel2.transform.rotation, Quaternion.Euler(0, 0, -120), step);
-        }
-        if (player.GetComponent<UNGA>().p2Arm == 3)
-        {
-            wheel2.transform.rotation = Quaternion.RotateTowards(wheel2.transform.rotation, Quaternion.Euler(0, 0, -240), step);
-        }
+            attack2.GetComponent<Image>().sprite = sprites[1];
+            jump2.GetComponent<Image>().sprite = sprites[2];
+            shield2.GetComponent<Image>().sprite = sprites[4];
+            }
+            if (player.GetComponent<UNGA>().p2Arm == 2)
+            {
+                attack2.GetComponent<Image>().sprite = sprites[0];
+                jump2.GetComponent<Image>().sprite = sprites[3];
+                shield2.GetComponent<Image>().sprite = sprites[4];
+            }
+            if (player.GetComponent<UNGA>().p2Arm == 3)
+            {
+                attack2.GetComponent<Image>().sprite = sprites[0];
+                jump2.GetComponent<Image>().sprite = sprites[2];
+                shield2.GetComponent<Image>().sprite = sprites[5];
+            }
 
-    }
+        }
 }
